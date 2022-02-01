8608ccc2d14e1c4cc7b4ae9163ee353a8eb431ca
AstridG1 <97997184+AstridG1@users.noreply.github.com>
2022-01-31 18:58:11 -0600

    Yaw and Pitch
    Pruebas con yaw y pitch. Agregando valores

---
+++ src/main/java/frc/robot/subsystems/Tecbotcamera.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.BooleanSupplier;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Tecbotcamera extends SubsystemBase {
+
+  PhotonCamera pepethefrog;
+  PhotonPipelineResult peperesult;
+  /** Creates a new Photonvision. */
+  public Tecbotcamera() {
+
+
+  pepethefrog= new PhotonCamera("Pepe.the.frog");
+  
+
+  }
+
+  @Override
+  public void periodic() {
+//System.out.println("kangaroo");
+
+    double yaw = 0;
+    double pitch = 0;
+
+    boolean hasTargets = pepethefrog.getLatestResult().hasTargets();
+
+    if(hasTargets == true){
+      peperesult = pepethefrog.getLatestResult();
+      PhotonTrackedTarget target = peperesult.getBestTarget();
+
+
+      yaw = target.getYaw();
+      pitch = target.getPitch();
+
+
+      
+    }
+    
+
+    // Shuffleboard.getTab("SmartDashboard").addBoolean(title, valueSupplier)
+
+    SmartDashboard.putBoolean("targets", hasTargets);
+    SmartDashboard.putNumber("yaw", yaw);
+    SmartDashboard.putNumber("pitch", pitch);
+  }
+}
